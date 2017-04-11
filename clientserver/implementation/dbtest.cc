b8344fef17e4c2a3369a5116d97eae6ae81e0ec8
m-tovar <marceldavidtovar@hotmail.com>
2017-04-11 07:39:10 +0200

    dbtest checks auto increment in newsgroup_id and global article counters. Updated gitignore.

--- clientserver/implementation/dbtest.cc
+++ clientserver/implementation/dbtest.cc
@@ -18,17 +18,23 @@ int main(int argc, char* argv[]){
 	db.list_ART(3);
 
 	db.create_NG("Good News Group");
+	db.create_NG("Bad News Group");
 	std::vector<Newsgroup> ngs = db.list_NG();
 	for (auto ng : ngs){
 		std::cout << ng.getTitle() << std::endl;
 	}
 	db.create_ART(1, "Today", "Marcel", "Sunday afternoon is pretty chill");
+	db.create_ART(2, "Tomorrow", "Lecram", "Monday morning sucks");
 	std::vector<Article> arts = db.list_ART(1);;
 	for (auto art : arts){
 		std::cout << art.getTitle() << std::endl;
 	}
 
 	Article art = db.get_ART(1, 1);
+	std::cout << art.getId() << " " << art.getTitle() << " " << art.getAuthor()
+	<< " " << art.getContent() << std::endl;
+
+	art = db.get_ART(2, 2);
 	std::cout << art.getId() << " " << art.getTitle() << " " << art.getAuthor()
 	<< " " << art.getContent() << std::endl;
 
