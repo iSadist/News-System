dedd68cd6acd617a9b47364a544457126dd389bb
Jan Svensson <jsvensson51@yahoo.com>
2017-04-16 22:15:10 +0200

    Add sql database
    Initialized with open connection and close connection methods. A database file is also added.

--- clientserver/implementation/sqldatabase.cc
+++ clientserver/implementation/sqldatabase.cc
@@ -1,9 +1,59 @@
 #include "sqldatabase.h"
 
+static int callback(void *data, int argc, char **argv, char **azColName){
+   int i;
+   fprintf(stderr, "%s: ", (const char*)data);
+   for(i=0; i<argc; i++){
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
+
 Sqldatabase::Sqldatabase(){
 
 }
 
 Sqldatabase::~Sqldatabase(){
 
+}
+
+void Sqldatabase::open_connection() {
+  rc = sqlite3_open("Database.db", &db);
+
+  if( rc ){
+     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+  }else{
+     fprintf(stderr, "Opened database successfully\n");
+  }
+}
+
+void Sqldatabase::close_connection() {
+  if (db) {
+    sqlite3_close(db);
+  }
+}
+
+bool Sqldatabase::create_NG(std::string title) {
+
+}
+
+bool Sqldatabase::delete_NG(int ng_id) {
+
+}
+
+std::vector<Article> list_ART(int ng_id) {
+
+}
+
+bool create_ART(int ng_id, std::string title, std::string author, std::string text) {
+
+}
+
+bool delete_ART(int ng_id, int art_id) {
+
+}
+
+Article get_ART(int ng_id, int art_id) {
+  
 }
--- clientserver/implementation/sqldatabase.h
+++ clientserver/implementation/sqldatabase.h
@@ -2,12 +2,30 @@
 #define SQLDATABASE_H
 
 #include "database.h"
+#include <sqlite3.h>
 
-class Sqldatabase : Database{
+class Sqldatabase : public Database {
+
+private:
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+	char *sql;
+	const char* data = "Callback function called";
+
+public:
 	Sqldatabase();
 	virtual ~Sqldatabase();
 
-};
+	void open_connection();
+	void close_connection();
 
+	bool create_NG(std::string title);
+	bool delete_NG(int ng_id);
+	std::vector<Article> list_ART(int ng_id);
+	bool create_ART(int ng_id, std::string title, std::string author, std::string text);
+	bool delete_ART(int ng_id, int art_id);
+	Article get_ART(int ng_id, int art_id);
+};
 
 #endif
