8749189d049eda86be17c79b9a9fbbb481707b77
Jan Svensson <jsvensson51@yahoo.com>
2017-04-18 13:06:42 +0200

    Finish SQL database
    Not tested yet!

--- clientserver/implementation/sqldatabase.cc
+++ clientserver/implementation/sqldatabase.cc
@@ -1,16 +1,6 @@
 #include "sqldatabase.h"
 #include "article.h"
 
-static int callback(void *data, int argc, char **argv, char **azColName){
-   int i;
-   fprintf(stderr, "%s: ", (const char*)data);
-   for(i=0; i<argc; i++){
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
-
 Sqldatabase::Sqldatabase(){
   article_counter = 0;
 }
@@ -26,10 +16,8 @@ bool prepareStatement(std::string sql) {
 
   if (sqlite3_prepare(db, query, -1, statement, 0) == SQLITE_OK) {
     result = sqlite3_step(statement);
-    if(result != 0) {
-      return true;
-    }
     sqlite3_finalize(statement);
+    if(result != 0) return true;
   }
   return false;
 }
@@ -59,7 +47,7 @@ bool Sqldatabase::create_NG(std::string title) {
 bool Sqldatabase::delete_NG(int ng_id) {
   //Delete relations
   std::string sql = "DELETE FROM contains " \
-                    "WHERE group_id = " + ng_id;
+                    "WHERE       group_id = " + ng_id;
   prepareStatement(sql);
 
   //Delete newsgroup
@@ -70,8 +58,26 @@ bool Sqldatabase::delete_NG(int ng_id) {
 
 std::vector<Article> list_ART(int ng_id) {
   std::vector<Article> articles;
-  
+  sqlite3_stmt *statement;
+  char *query = "SELECT id,title,author,content FROM articles " \
+                "INNER JOIN contains " \
+                "ON         id = article_id " \
+                "WHERE      group_id = " + ng_id;
 
+  if(sqlite3_prepare(db, query, -1, &statement, 0) == SQLITE_OK) {
+    int columns = sqlite3_column_count(statement);
+
+    while (sqlite3_step(statement) == SQLITE_ROW) {
+       int id = sqlite3_column_int(statement, 1);
+       std::string title = sqlite3_column_text(statement, 2);
+       std::string author = sqlite3_column_text(statement, 3);
+       std::string content = sqlite3_column_text(statement, 4);
+       Article art(id, title, author,content);
+       articles.push_back(art);
+    }
+  }
+
+  return articles;
 }
 
 bool create_ART(int ng_id, std::string title, std::string author, std::string text) {
@@ -88,11 +94,32 @@ bool create_ART(int ng_id, std::string title, std::string author, std::string te
 
 bool delete_ART(int ng_id, int art_id) {
   std::string sql = "DELETE FROM contains " \
-                    "WHERE article_id = " + art_id +
-                    " AND  group_id = " + ng_id;
+                    "WHERE       article_id = " + art_id +
+                    " AND        group_id = " + ng_id;
   return prepareStatement(sql);
 }
 
 Article get_ART(int ng_id, int art_id) {
+  Article article;
 
+  sqlite3_stmt *statement;
+  char *query = "SELECT id,title,author,content FROM articles " \
+                "INNER JOIN contains " \
+                "ON         id = article_id " \
+                "WHERE      group_id = " + ng_id
+              + " AND       article_id = " + art_id;
+  if(sqlite3_prepare(db, query, -1, &statement, 0) == SQLITE_OK) {
+    int columns = sqlite3_column_count(statement);
+
+    if (sqlite3_step(statement) == SQLITE_ROW) {
+      int id = sqlite3_column_int(statement, 1);
+      std::string title = sqlite3_column_text(statement, 2);
+      std::string author = sqlite3_column_text(statement, 3);
+      std::string content = sqlite3_column_text(statement, 4);
+      Article art(id, title, author,content);
+      article = art;
+    }
+  }
+
+  return article;
 }
