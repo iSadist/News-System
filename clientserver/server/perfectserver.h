4631187107fe4bf557961f6f06027798fb03958d
Jan Svensson <jsvensson51@yahoo.com>
2017-04-21 20:49:32 +0200

    Start server implementation

---
+++ clientserver/server/perfectserver.h
@@ -0,0 +1,20 @@
+#ifndef PERFECT_SERVER_H
+
+#define PERFECT_SERVER_H
+
+
+
+class PerfectServer : public Server{
+private:
+  Server server;
+
+public:
+  PerfectServer ();
+  virtual ~PerfectServer ();
+
+
+};
+
+
+
+#endif
